e57a0ecc2a22bd0117d70de388467d870ddd5bee
tyler-kearney <tyj376@gmail.com>
2025-12-04 00:36:29 -0600

    set up deploy

--- fantasy-backstory-generator/vite.config.js
+++ fantasy-backstory-generator/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
+
+const repoName = 'fantasy-backstory-generator';
 export default defineConfig({
   plugins: [svelte()],
+  base: `/${repoName}/`
 })
